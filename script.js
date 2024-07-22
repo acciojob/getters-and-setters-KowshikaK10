e04ff79d61720839852d2b62943dc2d3db80efea
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-22 11:11:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(n,a){
+		this.name = n;
+		this.age = a;
+	}
+	get getName(){
+		return this.name;
+	}
+	set setAge(value){
+		this.age=value;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
+const person=new Person("Ram",'20');
+console.log(person.name);
+
+person.age='30';
+console.log(person.age);
+
+const student = new Student("student1", 22);
+student.study();
+
+const teacher = new Teacher("teacher1", 40);
+teacher.teach(); 
 
 // Do not change the code below this line
 window.Person = Person;
