e04a3d1bbdcda91034c2559235cf0cecde5ffb57
KowshikaK10 <139222757+KowshikaK10@users.noreply.github.com>
2024-07-22 11:14:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ class Person {
 	get getName(){
 		return this.name;
 	}
+	
 	set setAge(value){
 		this.age=value;
 	}
@@ -23,17 +24,17 @@ class Teacher extends Person {
 		console.log(`${this.name} is teaching`);
 	}
 }
-const person=new Person("Ram",'20');
-console.log(person.name);
+const person = new Person("John", 25);
+console.log(person.name); // Output: John
 
-person.age='30';
-console.log(person.age);
+person.age = 30;
+console.log(person.age); // Output: 30
 
-const student = new Student("student1", 22);
-student.study();
+const student = new Student("Alice", 22);
+student.study(); // Output: Alice is studying
 
-const teacher = new Teacher("teacher1", 40);
-teacher.teach(); 
+const teacher = new Teacher("Bob", 40);
+teacher.teach(); // Output: Bob is teaching
 
 // Do not change the code below this line
 window.Person = Person;
